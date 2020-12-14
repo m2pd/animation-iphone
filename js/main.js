e501e90dd9966d5c86cc67ad4624bee8c6347cf1
Đoàn Minh Phượng <doanminhphuong.208@tcu.edu.vn>
2020-12-14 14:59:47 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+var tlFirstScroll = new TimelineMax();
+
+tlFirstScroll
+    .set('.iphone-image-wrapper', { scale: 4, transformOrigin: "center top" })
+    .to('.iphone-image-wrapper', 3, { scale: 2.2, y: "-50%" })
+    .to('.iphone-image-wrapper', 3, { scale: 1, y: "0%" })
+
+var controller = new ScrollMagic.Controller();
+
+//Scene 1
+var scene1 = new ScrollMagic.Scene({
+    triggerElement: '.trigger1', // Phần tử trigger
+    triggerHook: 0,//Vị trí bắt đầu của TriggerElement từ 0 -> 1 (ở đây là ".trigger1")
+    duration: "100%",//Cảnh sẽ kéo dài trong scroll ("end").Khi scroll hết "End" thì sẽ dừng
+})
+
+    .setTween(tlFirstScroll) //Thêm Tween vào Scene
+    .addIndicators() // Hiển thị thanh trigger
+    .addTo(controller) // Gán Scene cho controller. (Bắt buộc)
+
+
+var tlSecondScroll = new TimelineMax();
+
+tlSecondScroll
+    .to('.iphone1', 3, { x: "-54%" })
+    .to('.iphone2', 3, { x: "54%" }, "-=3")
+    .from('.iphone1-text', 0.3, { autoAlpha: 0 }, "-=3")
+    .from('.iphone2-text', 0.3, { autoAlpha: 0 }, "-=3")
+    .to('.iphone1-text', 3, { x: "-30%" }, "-=3")
+    .to('.iphone2-text', 3, { x: "30%" }, "-=3")
+
+    .set('.iphone-stick', { display: 'block' })
+
+    .to('.iphone1-img-behind', 3, { x: "-50%" })
+    .to('.iphone2-img-behind', 3, { x: "50%" }, "-=3")
+
+    .to('.iphone1-img', 0.5, { autoAlpha: 0 }, "-=3")
+    .to('.iphone2-img', 0.5, { autoAlpha: 0 }, "-=3")
+
+    .to('.iphone1-text', 0.3, { autoAlpha: 0 }, "-=3")
+    .to('.iphone2-text', 0.3, { autoAlpha: 0 }, "-=3")
+
+var scene2 = new ScrollMagic.Scene({
+    triggerElement: '.trigger2',
+    triggerHook: 0,
+    duration: "100%",
+})
+
+    .setTween(tlSecondScroll)
+    .setPin('.trigger2') // Ghim cho phàn tử scroll nếu "End" ở cuối khống scroll được nữa
+    .addIndicators()
+    .addTo(controller)
